4a4d092e2eaa3e594ed8782418bd45b09d3b01ba
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-10-08 20:11:30 +0200

    spring boot properties

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/TestRest.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/TestRest.java
@@ -1,10 +1,26 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRest {
+
+    //Inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teamInfo"
+
+    @GetMapping("/teamInfo")
+    public String getTeamIfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String hello(){
